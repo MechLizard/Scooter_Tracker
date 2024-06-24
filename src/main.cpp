ac104ee0eea6e745990bba2cb4be110318d72898
qwasmos <qwasmos0102@gmail.com>
2024-06-24 17:36:25 -0400

    I have added very basic, very primitive ability to generate random data for the accounts, each booking will have an account as well. I also only added a single account, but the permissions of the accounts are differentiated by an enum given specific permissions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 enum Permissions{
     ADMIN,
@@ -24,13 +25,50 @@ struct Booking{
 };
 
 
+void generateAccounts(std::vector<Account>& vector, unsigned int numberOfAccounts){
+    char alph[26];
 
+
+    for(unsigned int i = 0; i < 26;i++){
+	alph[i] = 'A'+i;
+    }
+
+
+
+    for(unsigned int i = 0; i < numberOfAccounts;i++){
+	Account account;
+	for(unsigned int i = 0; i < 10;i++){
+
+	    account.email.push_back(alph[rand() % 26]);
+	    account.password.push_back(alph[rand() % 26]);
+	    if(account.permission == BUSINESS){
+		account.nameOfBusiness.push_back(alph[rand() % 26]);
+	    }
+
+	}
+	vector.push_back(account);
+	
+    }
+
+}
+
+
+void printAccounts(std::vector<Account>& vec){
+    for(auto& i: vec){
+	std::cout<<i.email<<std::endl;
+    }
+
+}
 
 
 int main(){
     std::cout<<"Hello World"<<std::endl;
+    std::vector<Account> vec;
+    generateAccounts(vec,155);
 
-    Booking booking = {};
-    std::cout<<booking.account.permission<<std::endl;
+    printAccounts(vec);
+    
+
+    
 
 }
